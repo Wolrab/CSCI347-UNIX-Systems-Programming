ab37b6d4d61ce4bfc1fca39b61b33da227dbb702
Connor Barlow <barlowc2@wwu.edu>
2019-11-01 18:01:01 -0700

    First refactor done

--- autotools/find_src/expression_primaries.c
+++ autotools/find_src/expression_primaries.c
@@ -114,7 +114,7 @@ int get_primary_globals(primary_args_g *global_args) {
 
 /**
  * Takes a primary, its arg value, and globals, and returns its truth value
- *   for the given f_stat.
+ *   for the given file tree entry.
  * Returns: the truth value for the given primary.
  * In the case that the primary doesn't exist, the program aborts.
  */
@@ -123,22 +123,23 @@ bool primary_evaluate(primary_t primary, primary_arg *arg,\
     bool ret = false;
     switch(primary) {
     case CNEWER:
-        ret = eval_cnewer(entry->fts_statp, arg->stat_arg);
+        ret = eval_cnewer(&(entry->fts_statp->st_ctim), \
+                          &(arg->stat_arg->st_ctim));
         break;
     case CMIN:
-        ret = eval_cmin(entry->fts_statp, arg->long_arg, globals);
+        ret = eval_cmin(&(entry->fts_statp->st_ctim), arg->long_arg, globals);
         break;
     case CTIME:
-        ret = eval_ctime(entry->fts_statp, arg->long_arg, globals);
+        ret = eval_ctime(&(entry->fts_statp->st_ctim), arg->long_arg, globals);
         break;
     case MMIN:
-        ret = eval_mmin(entry->fts_statp, arg->long_arg, globals);
+        ret = eval_mmin(&(entry->fts_statp->st_mtim), arg->long_arg, globals);
         break;
     case MTIME:
-        ret = eval_mtime(entry->fts_statp, arg->long_arg, globals);
+        ret = eval_mtime(&(entry->fts_statp->st_mtim), arg->long_arg, globals);
         break;
     case TYPE:
-        ret = eval_type(entry->fts_statp, arg->char_arg);
+        ret = eval_type(entry->fts_statp->st_mode, arg->char_arg);
         break;
     case PRIMARY_NUM:
         abort();
@@ -152,17 +153,18 @@ bool primary_evaluate(primary_t primary, primary_arg *arg,\
 /**
  * Returns true if f_stat is newer than o_stat, false otherwise.
  */
-bool eval_cnewer(struct timespec *f_time, struct timespec *o_time) {
-    return f_time->tv_sec > o_time->tv_sec || \
-        (f_time->tv_sec == o_time->tv_sec && f_time->tv_nsec > o_time->tv_nsec);
+bool eval_cnewer(struct timespec *ctim, struct timespec *o_ctim) {
+    return ctim->tv_sec > o_ctim->tv_sec || \
+        (ctim->tv_sec == o_ctim->tv_sec && \
+         ctim->tv_nsec > o_ctim->tv_nsec);
 }
 
 /**
  * Returns true if the last change of file status information was n minutes
  *   ago (rounded up), false otherwise.
  */
-bool eval_cmin(struct stat *f_stat, long n, primary_args_g *global_args) {
-    time_t min = f_stat->st_ctim.tv_sec / SEC_PER_MIN;
+bool eval_cmin(struct timespec *ctim, long n, primary_args_g *global_args) {
+    time_t min = ctim->tv_sec / SEC_PER_MIN;
     return global_args->time_min - min == n;
 }
 
@@ -170,8 +172,8 @@ bool eval_cmin(struct stat *f_stat, long n, primary_args_g *global_args) {
  * Returns true if the last change of file status information was n days
  *   ago (rounded up), false otherwise.
  */
-bool eval_ctime(struct stat *f_stat, long n, primary_args_g *global_args) {
-    time_t day = f_stat->st_ctim.tv_sec / SEC_PER_DAY;
+bool eval_ctime(struct timespec *ctim, long n, primary_args_g *global_args) {
+    time_t day = ctim->tv_sec / SEC_PER_DAY;
     return global_args->time_day - day == n;
 }
 
@@ -179,8 +181,8 @@ bool eval_ctime(struct stat *f_stat, long n, primary_args_g *global_args) {
  * Returns true if the last file modification was n minutes ago (rounded up),
  *   false otherwise.
  */
-bool eval_mmin(struct stat *f_stat, long n, primary_args_g *global_args) {
-    time_t min = f_stat->st_mtim.tv_sec / SEC_PER_MIN;
+bool eval_mmin(struct timespec *mtim, long n, primary_args_g *global_args) {
+    time_t min = mtim->tv_sec / SEC_PER_MIN;
     return global_args->time_min - min == n;
 }
 
@@ -188,8 +190,8 @@ bool eval_mmin(struct stat *f_stat, long n, primary_args_g *global_args) {
  * Returns true if the last file modification was n days ago (rounded up),
  *   false otherwise.
  */
-bool eval_mtime(struct stat *f_stat, long n, primary_args_g *global_args) {
-    time_t day = f_stat->st_mtim.tv_sec / SEC_PER_DAY;
+bool eval_mtime(struct timespec *mtim, long n, primary_args_g *global_args) {
+    time_t day = mtim->tv_sec / SEC_PER_DAY;
     return global_args->time_day - day == n;
 }
 
@@ -197,8 +199,8 @@ bool eval_mtime(struct stat *f_stat, long n, primary_args_g *global_args) {
  * Returns true if the character representation of f_stat->st_mode is
  *   equivalent to t, false otherwise.
  */
-bool eval_type(struct stat *f_stat, char t) {
-    return get_type_char(f_stat->st_mode) == t;
+bool eval_type(mode_t mode, char t) {
+    return get_type_char(mode) == t;
 }
 
 // Helpers for primary evaluation functions
--- autotools/find_src/expression_primaries.h
+++ autotools/find_src/expression_primaries.h
@@ -69,12 +69,12 @@ bool primary_evaluate(primary_t primary, primary_arg *arg,\
     primary_args_g *globals, FTSENT *entry);
 
 // Primary evaluation functions
-bool eval_cnewer(struct timespec *f_time, struct timespec *o_time);
-bool eval_cmin(struct stat *f_stat, long n, primary_args_g *global_args);
-bool eval_ctime(struct stat *f_stat, long n, primary_args_g *global_args);
-bool eval_mmin(struct stat *f_stat, long n, primary_args_g *global_args);
-bool eval_mtime(struct stat *f_stat, long n, primary_args_g *global_args);
-bool eval_type(struct stat *f_stat, char t);
+bool eval_cnewer(struct timespec *ctim, struct timespec *o_ctim);
+bool eval_cmin(struct timespec *ctim, long n, primary_args_g *global_args);
+bool eval_ctime(struct timespec *ctim, long n, primary_args_g *global_args);
+bool eval_mmin(struct timespec *mtim, long n, primary_args_g *global_args);
+bool eval_mtime(struct timespec *mtim, long n, primary_args_g *global_args);
+bool eval_type(mode_t mode, char t);
 
 // Helpers for primary evaluation functions
 char get_type_char(mode_t mode);
